0a515eb50c17953131d7d1af8166e705b00a6ba2
Chris Brown <christopher.brown@knowink.com>
2022-12-06 15:03:18 -0700

    Added Package File

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StarPRNT",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(name: "StarPRNT", targets: ["StarIO","StarIO_Extension"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "StarIO", path: "./SDK/Pods/StarIO/StarIO.xcframework"),
+        .binaryTarget(name: "StarIO_Extension", path: "./SDK/Pods/StarIO_Extension/StarIO_Extension.xcframework"),
+    ]
+)
+
+
